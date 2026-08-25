66e2486a74b322dec83ff000f79dfbc9741c3810
ProjectVoul <pesi.marco.info@gmail.com>
2026-08-25 11:56:47 +0300

    remove: delete legacy ruleset configuration

--- src/domain/ruleset.ts
+++
@@ -1,2 +0,0 @@
-export interface RulesetConfig{edition:'5e-2014';sources:string[];allowTashaOptionalFeatures:boolean;allowXanatharContent:boolean;allowHomebrew:boolean;}
-export const DEFAULT_RULESET:RulesetConfig={edition:'5e-2014',sources:['phb2014','xge2017'],allowTashaOptionalFeatures:true,allowXanatharContent:true,allowHomebrew:true};
